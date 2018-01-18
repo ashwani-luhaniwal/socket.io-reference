b10d4a5b8a71d792ed0d074fefd554e990e0ff18
ashwani-luhaniwal <contact.ashwin.cse@gmail.com>
2018-01-17 22:34:49 -0800

    Event Handling in Socket.IO

---
+++ documentation/Event Handling/Events.js
@@ -0,0 +1,21 @@
+/**
+ * ---------------------------
+ * Socket.IO - Event Handling
+ * ---------------------------
+ * Sockets work based on events. There are some reserved events, which can be accessed using the socket 
+ * object on the server side.
+ *  These are −
+ *      Connect
+ *      Message
+ *      Disconnect
+ *      Reconnect
+ *      Ping
+ *      Join
+ *      Leave
+ * 
+ * The client-side socket object also provides us with some reserved events, which are −
+ *      Connect
+ *      Connect_error
+ *      Connect_timeout
+ *      Reconnect
+ */
